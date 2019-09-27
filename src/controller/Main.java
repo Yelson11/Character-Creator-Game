634412d040a40b1182bf0724c598086889141476
Carlos Joel <carlosvega124@gmail.com>
2019-09-27 05:50:57 -0600

    Cambio de interfaz

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,31 +1,10 @@
 package controller;
 
-import java.awt.Image;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import javax.imageio.ImageIO;
-import library.Character.*;
-import library.Character;
-import library.CharacterFactory;
-import library.IPrototype;
-import library.Weapon;
-import library.Weapon.WeaponBuilder;
-import library.WeaponFactory;
-import view.SelectCharacterWindow;
 import view.SelectCharacterWindow;
 
 public class Main {
     public static void main(String args[]) throws IOException{
-        
-        IPrototype c1 = new CharacterBuilder().addName("C1").addImage("amaterasu").build();
-        IPrototype c2 = new CharacterBuilder().addName("C2").addImage("amaterasu").build();
-        IPrototype c3 = new CharacterBuilder().addName("C3").addImage("amaterasu").build();
-        CharacterFactory.addPrototype("x", c1);
-        CharacterFactory.addPrototype("y", c2);
-        CharacterFactory.addPrototype("z", c3);
-        Character c4 = (Character) CharacterFactory.getValues().get(0);
-        System.out.println(c4.getName());
         
         SelectCharacterWindow ui = new SelectCharacterWindow();
         ui.show();
--- src/view/SelectCharacterWindow.java
+++ src/view/SelectCharacterWindow.java
@@ -44,7 +44,7 @@ public class SelectCharacterWindow extends javax.swing.JFrame{
     public void paint(Graphics g){
         g.drawImage(background, 0, 0, this);
         controller.draw(g);
-        jButton1.updateUI();
+        btnRight.updateUI();
     }
 
     /**
@@ -56,14 +56,14 @@ public class SelectCharacterWindow extends javax.swing.JFrame{
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        btnRight = new javax.swing.JButton();
+        btnLeft = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnRight.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnRightActionPerformed(evt);
             }
         });
 
@@ -72,28 +72,32 @@ public class SelectCharacterWindow extends javax.swing.JFrame{
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(550, Short.MAX_VALUE)
-                .addComponent(jButton1))
+                .addContainerGap()
+                .addComponent(btnLeft, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 486, Short.MAX_VALUE)
+                .addComponent(btnRight, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(184, 184, 184)
-                .addComponent(jButton1)
-                .addContainerGap(209, Short.MAX_VALUE))
+                .addGap(178, 178, 178)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnLeft, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRight, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(219, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnRightActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRightActionPerformed
         try {
             controller.changeImage(1);
         } catch (IOException ex) {
             Logger.getLogger(SelectCharacterWindow.class.getName()).log(Level.SEVERE, null, ex);
         }
         repaint();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnRightActionPerformed
 
     /**
      * @param args the command line arguments
@@ -138,6 +142,7 @@ public class SelectCharacterWindow extends javax.swing.JFrame{
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnLeft;
+    private javax.swing.JButton btnRight;
     // End of variables declaration//GEN-END:variables
 }
